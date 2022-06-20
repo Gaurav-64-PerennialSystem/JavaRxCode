c93af58d3cd74457b4e509040859d2c67b419ba9
Gaurav Sarodkar <99338400+Gaurav64646464@users.noreply.github.com>
2022-06-20 19:09:09 +0530

    Rx java code

---
+++ TakeWhileSkipWhileOperator.java
@@ -0,0 +1,59 @@
+import io.reactivex.Observable;
+
+import java.util.concurrent.TimeUnit;
+
+public class TakeWhileSkipWhileOperator {
+    
+    
+    public static void main(String[] args) {
+      //  takeOperator();
+       // takeWithInterval();
+   // takeWhileOperator();
+   // skipOperator();
+    skipWhileOperator();
+    }
+    
+    private static void takeOperator() {
+        Observable.just(1,2,3,4,5)
+                .take(2)
+                .subscribe(System.out::println);
+    }
+    
+    private static void skipWhileOperator() {
+        Observable.just(1,2,3,4,5,1,2,3,4,5)
+                .skipWhile(item-> {
+                    return item <= 3;
+                })
+                .subscribe(System.out::println);
+    }
+    
+    private static void skipOperator() {
+       Observable.just(1,2,3,4,5)
+               .skip(2).subscribe(System.out::println);
+    }
+    
+    private static void takeWhileOperator() {
+    Observable.just(1,2,3,4,5,1,2,3,4,5)
+            .filter(item->item<=3)
+            .subscribe(System.out::println);
+    
+    }
+    
+    private static void takeWithInterval() {
+    Observable.interval(300, TimeUnit.MILLISECONDS)
+            .take(2,TimeUnit.SECONDS)
+            .subscribe(System.out::println,
+                    System.out::println,()-> System.out.println("Completed"));
+   pause(5000);
+    }
+    
+    private static void pause(int duration) {
+    
+        try {
+            Thread.sleep(duration);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+    
+}
